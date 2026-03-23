3674281c6eea847db06ea370ef7cb10943e894d3
alexa <alexandrecarmo.t@hotmail.com>
2026-03-23 18:46:04 -0300

    Versão 0.0.3
    - Build

--- src/main/java/org/onosproject/drivers/padtec/PadtecAlarmConsumer.java
+++ src/main/java/org/onosproject/drivers/padtec/PadtecAlarmConsumer.java
@@ -1,29 +1,7 @@
 package org.onosproject.drivers.padtec;
 
-import org.onosproject.alarm.Alarm;
-import org.onosproject.alarm.AlarmConsumer;
-import org.onosproject.net.driver.AbstractHandlerBehaviour;
-import org.slf4j.Logger;
-
-import java.util.Collections;
-import java.util.List;
-
-import static org.slf4j.LoggerFactory.getLogger;
-
 /**
- * Alarm Consumer for Padtec devices.
+ * Unused in current version.
  */
-public class PadtecAlarmConsumer extends AbstractHandlerBehaviour implements AlarmConsumer {
-
-    private final Logger log = getLogger(getClass());
-
-    @Override
-    public List<Alarm> consumeAlarms() {
-        log.info("Consuming alarms for Padtec device {}...", handler().data().deviceId());
-        
-        // Aqui no futuro você pode integrar com o Jaquison para buscar as falhas de equipamento
-        // e converter para a classe Alarm do ONOS.
-        
-        return Collections.emptyList();
-    }
+public class PadtecAlarmConsumer {
 }
